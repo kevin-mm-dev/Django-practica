6863d55a8fb7b7d9cab1e170613ebdd4b8f3ebd3
kevin-mm-dev <kevin.mm.dev@gmail.com>
2020-08-11 08:54:21 -0500

    Ante de rest

--- services/views.py
+++ services/views.py
@@ -1,10 +1,57 @@
 from django.shortcuts import render
 from services.models import Products
 
+
 # Create your views here.
 def showTemplate(request):
     response = Products.objects.all()
     return render(request, 'index.html', {'response' : response})
+
+def createSubject(request):
+    response = Products.objects.all()
+    if request.method=="POST":
+        product=Products.objects.create(
+            name=request.POST["subject_name"],
+            category=request.POST["cate"],
+            ubication=request.POST["ubi"],
+            lote=request.POST["lote"]
+        )
+        product.save()
+        # return redirect('index')
+        return render(request, 'index.html', {'response' : response})
+    return render(request, 'create.html')
+    # print(request,POST)
+    # return render(request, 'create.html')
+
+# def editSubject(request):
+#     response = Products.objects.all()
+#     product=Products.objects.(
+#         name=request.POST["subject_name"],
+#         category=request.POST["cate"],
+#         ubication=request.POST["ubi"],
+#         lote=request.POST["lote"]
+#     )
+#     product.save()
+#     # return redirect('index')
+#     return render(request, 'index.html', {'response' : response})
+#     # return render(request, 'create.html')
+
+
+
+    # if request.method=="POST":
+    #     product=Products.objects.create(
+    #         name=request.POST["subject_name"],
+    #         category=request.POST["cate"],
+    #         ubication=request.POST["ubi"],
+    #         lote=request.POST["lote"]
+
+    #     )
+    #     product.save()
+    #     # return redirect('index')
+    #     return render(request, 'index.html', {'response' : response})
+    # return render(request, 'create.html')
+    # print(request,POST)
+    # return render(request, 'create.html')
 
 #pip freeze > requeriments.txt
 #pip install -r requeriments.txt
